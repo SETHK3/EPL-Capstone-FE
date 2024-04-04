ad73c23e6e65741c93b21ad310171476db401a14
Seth Gartz <threegartz@gmail.com>
2024-04-04 16:00:19 -0600

    added mixins and variables, navbar and footer styles

---
+++ src/components/nav/NavBar.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+
+// import logo from "../../assets/images/2950-fixed.png";
+
+export default function NavBar() {
+  return (
+    <div className="navbar-container">
+      <div className="navbar-wrapper">
+        <div className="navbar-logo">
+          {/* <img src={logo} alt="2950 logo" /> */}
+        </div>
+        <div className="navbar-links">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/products">Teams</NavLink>
+          <NavLink to="/cart">Managers</NavLink>
+          <NavLink to="/contact">Players</NavLink>
+          <NavLink to="/about">Stats</NavLink>
+        </div>
+      </div>
+    </div>
+  );
+}
