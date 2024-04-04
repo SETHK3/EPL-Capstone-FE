d112055c42ddbda15ec4c5911bdc268b9e12120b
Seth Gartz <threegartz@gmail.com>
2024-04-04 17:23:16 -0600

    added logo img to home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,10 @@
+import epl_logo from "../../assets/images/epl-logo.png";
+
 export default function Home() {
   return (
     <div className="home-page page-container">
       <h1>English Premier League</h1>
+      <img src={epl_logo} alt="epl-logo" />
 
       <div className="home-content-wrapper">
         <div className="home-grid">
@@ -15,6 +18,18 @@ export default function Home() {
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
               allowFullScreen
+            ></iframe>
+          </div>
+          <div className="home-image">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/D0hhABqMFb8?si=UhQC41V2MO3P4L5j&amp;start=541"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
             ></iframe>
           </div>
         </div>
