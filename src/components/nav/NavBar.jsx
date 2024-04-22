d51e4ad6138a0b6a5010dcb8f153e6b23a31179b
Seth Gartz <threegartz@gmail.com>
2024-04-22 00:06:16 -0600

    created dropdown profile menu

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { FaAngleDown } from "react-icons/fa";
 
 import logo from "../../assets/images/epl-logo-2.png";
 
@@ -15,9 +16,16 @@ export default function NavBar() {
           <NavLink to="/managers">Managers</NavLink>
           <NavLink to="/players">Players</NavLink>
           <NavLink to="/stats">Stats</NavLink>
-          <NavLink to="/signup">Create Account</NavLink>
-          <NavLink to="/profile">My Profile</NavLink>
-          <NavLink to="/login">Login</NavLink>
+          <div className="navbar-dropdown">
+            <NavLink to="/profile" className="dropdown-btn">
+              Profile <FaAngleDown className="dropdown-arrow" />
+            </NavLink>
+            <div className="dropdown-content">
+              <NavLink to="/signup">Create Account</NavLink>
+              <NavLink to="/profile">My Profile</NavLink>
+              <NavLink to="/login">Login</NavLink>
+            </div>
+          </div>
         </div>
       </div>
     </div>
